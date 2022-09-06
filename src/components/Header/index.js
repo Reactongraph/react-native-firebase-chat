e912237350b9261e8fa2b091d5a7dda5d6b7b7e1
Reactongraph <js.developer@ongraph.com>
2022-09-06 17:49:04 +0530

    changes

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,8 @@
-import React from 'react';
-import { Text, View } from 'react-native';
-import styles from './styles';
+import React from "react";
+import { Text, View } from "react-native";
+import styles from "./styles";
 
-type Props = {
-  title: string,
-};
-
-const Header = (props: Props) => (
+const Header = (props) => (
   <View style={styles.container}>
     <Text style={styles.title}>{props.title}</Text>
   </View>
